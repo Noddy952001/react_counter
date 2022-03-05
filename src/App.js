c6eb23cb3845f1804cfa6862c0d7cde1932d4a51
Vikas gupta <96105383+Noddy952001@users.noreply.github.com>
2022-03-05 18:00:22 +0530

    final_touch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  let [count, setCount] = useState(0);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='counter'>
+      <h1>Your Score :  {count}</h1>
+      <button onClick={() => setCount(count + 1)} className="btn">
+        Increment
+      </button>
+
+      <button onClick={() => setCount(count -1)}className="btn" >
+        Decrement
+      </button>
+
+      <button onClick={() => setCount(count*2)} className="btn" >
+        Double
+      </button>
     </div>
   );
 }
